ce7b082dcb6478a94ffa6abe6ff89c7ab9ab7b1c
Nicolás <nicolascesarini94@hotmail.com>
2024-10-29 00:42:51 -0300

    Vamos de vuelta

--- Escenario00/Observador.java
+++
@@ -1,9 +0,0 @@
-/**
- * Write a description of interface Observador here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public interface Observador {
-    void actualizar(boolean estado);
-}
--- Escenario00/PilotoRecolector.java
+++
@@ -1,8 +0,0 @@
-public interface PilotoRecolector  
-{
-    public void subirse(NaveRecolectoraOro nave);
-    
-    public void bajarse();
-    
-    public void actualizarImagen();
-}
